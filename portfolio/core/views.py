2860b1a0a3931171cf60a80fc9bec91d0caaca8e
AndresGonzaloGarcia <garciaandresgonzalo@gmail.com>
2024-01-09 14:01:48 -0300

    add templates and navbar

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def portfolio(request):
+    return render(request, 'portfolio.html', {})
+
+def about_me(request):
+    return render(request, 'about_me.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -5,6 +5,9 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name= 'home')
+    path('', views.home, name= 'home'),
+    path('portfolio/', views.portfolio, name= 'portfolio'),
+    path('about_me/', views.about_me, name= 'about_me'),
+    path('contact/', views.contact ,name= 'contact'),
     
 ]
